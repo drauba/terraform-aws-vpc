7c0da5863900a81062d6a44be1798b67a92883e5
drauba <drauba@forsythe.com>
2019-02-01 23:57:28 -0600

    Update main.tf

--- examples/complete-vpc/main.tf
+++ examples/complete-vpc/main.tf
@@ -23,7 +23,7 @@ module "vpc" {
   create_database_subnet_group = false
 
   enable_dns_hostnames = true
-  enable_dns_support   = tue
+  enable_dns_support   = true
 
   enable_nat_gateway = true
   single_nat_gateway = true
